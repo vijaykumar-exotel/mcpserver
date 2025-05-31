14d0f75e23d502902616460b7d49b7ded6fbae8d
vijay kumar <vijay.kumar@exotel.in>
2025-05-31 07:57:36 +0530

    changes

--- mcpinterfacenew.py
+++ mcpinterfacenew.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 import openaitest
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <- allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # <- allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # <- allows all headers
+)
 
 @app.get("/prompt")
 async def read_item(q: str | None = None):
