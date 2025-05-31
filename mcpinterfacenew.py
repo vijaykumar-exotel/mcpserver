2f059c52d50d5c251d9329c01898abd84d8933a0
vijay kumar <vijay.kumar@exotel.in>
2025-05-31 12:50:41 +0530

    test

--- mcpinterfacenew.py
+++ mcpinterfacenew.py
@@ -18,3 +18,20 @@ async def read_item(q: str | None = None):
         return {"status": "sucess", "data": data}
     return {"status": "failed"}
 
+
+@app.get("/call")
+async def read_item(q: str | None = None):
+    number = "07411179773"
+    url = f"https://399117e47411d9f0f9120de1181323056e55b88c664d2f67:80711a9d4562955dc3591f1ada24790f3b5088dbaa3263db@api.in.exotel.com/v1/Accounts/ameyo5m/Calls/connect.json?From={number}&Url=http://my.in.exotel.com/ameyo5m/exoml/start_voice/24049&CallerId=02247788868"
+    headers = {
+        'Authorization': 'Basic e3tBdXRoS2V5fX06e3tBdXRoVG9rZW59fQ==',
+        'Content-Type' : 'application/json'
+    }
+    #response = requests.request("POST", url, headers=headers)
+    headers = ["-H", "Content-Type: application/json"]
+    result = subprocess.run(
+        ["curl", "-s", "-X", "POST", url] + headers ,
+        capture_output=True,
+        text=True
+    )
+    return {"status": "success"}
